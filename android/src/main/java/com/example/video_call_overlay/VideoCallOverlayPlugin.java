88bbc486b57c7bd94c2619b0e006faf0bfd20f3e
JoseFrancisco93 <jfvc.93@gmail.com>
2023-06-28 19:36:37 +0000

    show widget floating

--- android/src/main/java/com/example/video_call_overlay/VideoCallOverlayPlugin.java
+++ android/src/main/java/com/example/video_call_overlay/VideoCallOverlayPlugin.java
@@ -1,6 +1,21 @@
 package com.example.video_call_overlay;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.graphics.PixelFormat;
+import android.view.View;
+import android.view.WindowManager;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -9,27 +24,47 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
 
-import android.content.Context;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.graphics.Color;
-import android.os.Build;
-
-import java.util.Random;
-
-public class VideoCallOverlayPlugin implements FlutterPlugin, MethodCallHandler {
+public class VideoCallOverlayPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware, ActivityResultListener {
     private MethodChannel channel;
     private Context applicationContext;
     private static final String CHANNEL_ID = "video_call_overlay_channel";
     private static final int NOTIFICATION_ID = 123;
+    private static final int OVERLAY_PERMISSION_REQUEST_CODE = 100;
+    private Activity activity;
+    private WindowManager.LayoutParams params;
+    private View overlayView;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPlugin.FlutterPluginBinding flutterPluginBinding) {
         applicationContext = flutterPluginBinding.getApplicationContext();
         channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "video_call_overlay");
         channel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+        binding.addActivityResultListener(this);
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        activity = null;
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+        binding.addActivityResultListener(this);
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        activity = null;
     }
 
     @Override
@@ -49,7 +84,30 @@ public class VideoCallOverlayPlugin implements FlutterPlugin, MethodCallHandler 
     }
 
     private void showNotificationOverlay(String notificationText) {
-        if (applicationContext != null) {
+        if (activity != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(applicationContext)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + applicationContext.getPackageName()));
+                activity.startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST_CODE);
+                return;
+            }
+
+            params = new WindowManager.LayoutParams(
+                    120,
+                    60,
+                    Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ? WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                            : WindowManager.LayoutParams.TYPE_PHONE,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    PixelFormat.TRANSPARENT);
+
+            overlayView = new View(applicationContext);
+            overlayView.setBackgroundColor(Color.BLACK);
+
+            WindowManager windowManager = (WindowManager) applicationContext.getSystemService(Context.WINDOW_SERVICE);
+            if (windowManager != null) {
+                windowManager.addView(overlayView, params);
+            }
+
             createNotificationChannel();
             NotificationCompat.Builder builder = new NotificationCompat.Builder(applicationContext, CHANNEL_ID)
                     .setSmallIcon(R.drawable.ic_call_black)
@@ -57,7 +115,6 @@ public class VideoCallOverlayPlugin implements FlutterPlugin, MethodCallHandler 
                     .setContentText(notificationText)
                     .setPriority(NotificationCompat.PRIORITY_HIGH)
                     .setOngoing(true);
-            ;
 
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(applicationContext);
             notificationManager.notify(NOTIFICATION_ID, builder.build());
@@ -74,12 +131,19 @@ public class VideoCallOverlayPlugin implements FlutterPlugin, MethodCallHandler 
             channel.enableLights(true);
             channel.setLightColor(Color.RED);
             NotificationManager notificationManager = applicationContext.getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
         }
     }
 
     private void closeNotificationOverlay() {
-        if (applicationContext != null) {
+        if (activity != null) {
+            WindowManager windowManager = (WindowManager) applicationContext.getSystemService(Context.WINDOW_SERVICE);
+            if (windowManager != null && overlayView != null) {
+                windowManager.removeView(overlayView);
+            }
+
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(applicationContext);
             notificationManager.cancel(NOTIFICATION_ID);
         }
@@ -89,5 +153,22 @@ public class VideoCallOverlayPlugin implements FlutterPlugin, MethodCallHandler 
     public void onDetachedFromEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
         channel.setMethodCallHandler(null);
         applicationContext = null;
+    }
+
+    @Override
+    public boolean onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQUEST_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(activity)) {
+                // Permiso concedido, puedes continuar con la lógica para mostrar la
+                // superposición
+                // ...
+            } else {
+                // Permiso no concedido, puedes mostrar un mensaje al usuario o realizar alguna
+                // acción adicional
+                // ...
+            }
+            return true;
+        }
+        return false;
     }
 }
