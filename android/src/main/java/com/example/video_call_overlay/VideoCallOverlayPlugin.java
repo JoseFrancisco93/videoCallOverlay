7d8fcfaba6d44f7f2430dfccba63e879324c0eb9
JoseFrancisco93 <jfvc.93@gmail.com>
2023-06-28 16:47:25 +0000

    showNotification Android

--- android/src/main/java/com/example/video_call_overlay/VideoCallOverlayPlugin.java
+++ android/src/main/java/com/example/video_call_overlay/VideoCallOverlayPlugin.java
@@ -1,6 +1,8 @@
 package com.example.video_call_overlay;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
@@ -8,17 +10,24 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
+import android.content.Context;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
+
+import java.util.Random;
+
 public class VideoCallOverlayPlugin implements FlutterPlugin, MethodCallHandler {
-    // The MethodChannel that will the communication between Flutter and native
-    // Android
-    //
-    // This local reference serves to register the plugin with the Flutter Engine
-    // and unregister it
-    // when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
+    private Context applicationContext;
+    private static final String CHANNEL_ID = "video_call_overlay_channel";
+    private static final int NOTIFICATION_ID = 123;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPlugin.FlutterPluginBinding flutterPluginBinding) {
+        applicationContext = flutterPluginBinding.getApplicationContext();
         channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "video_call_overlay");
         channel.setMethodCallHandler(this);
     }
@@ -27,13 +36,58 @@ public class VideoCallOverlayPlugin implements FlutterPlugin, MethodCallHandler 
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
         if (call.method.equals("getPlatformVersion")) {
             result.success("Version de Android " + android.os.Build.VERSION.RELEASE);
+        } else if (call.method.equals("showOverlay")) {
+            String notificationText = call.argument("notificationText");
+            showNotificationOverlay(notificationText);
+            result.success(true);
+        } else if (call.method.equals("closeOverlay")) {
+            closeNotificationOverlay();
+            result.success(true);
         } else {
             result.notImplemented();
+        }
+    }
+
+    private void showNotificationOverlay(String notificationText) {
+        if (applicationContext != null) {
+            createNotificationChannel();
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_call_black)
+                    .setContentTitle("En video llamada")
+                    .setContentText(notificationText)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setOngoing(true);
+            ;
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(applicationContext);
+            notificationManager.notify(NOTIFICATION_ID, builder.build());
+        }
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Video llamada";
+            String description = "Estás en una llamada con";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            NotificationManager notificationManager = applicationContext.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void closeNotificationOverlay() {
+        if (applicationContext != null) {
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(applicationContext);
+            notificationManager.cancel(NOTIFICATION_ID);
         }
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
         channel.setMethodCallHandler(null);
+        applicationContext = null;
     }
 }
